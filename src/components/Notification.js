1e19258881672a5611fc651b2226777631a3d16e
pbohora <pradeepbohora26@gmail.com>
2020-02-17 00:23:10 +0200

    fix notificatiob component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Alert, AlertTitle } from '@material-ui/lab'
 import { makeStyles } from '@material-ui/core/styles'
+import Snackbar from '@material-ui/core/Snackbar'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    marginTop: theme.spacing(8),
-    justifyContent: 'center'
-  }
-}))
 const Notification = ({ message, severity }) => {
-  const classes = useStyles()
   return (
     message && (
-      <Alert variant='filled' className={classes.root} severity={severity}>
-        <AlertTitle>{severity}</AlertTitle>
-        {message}
-      </Alert>
+      <Snackbar
+        open='open'
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+        <Alert variant='filled' severity={severity}>
+          <AlertTitle>{severity}</AlertTitle>
+          {message}
+        </Alert>
+      </Snackbar>
     )
   )
 }
