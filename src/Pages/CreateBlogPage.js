b11af2f26437f6f042612e8e7d499c1b61fa6871
pbohora <pradeepbohora26@gmail.com>
2020-02-20 22:19:15 +0200

    add grid component

--- src/Pages/CreateBlogPage.js
+++ src/Pages/CreateBlogPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
+
 import Layout from '../components/Layout'
 import Section from '../components/Section/Section'
 import BlogpostForm from '../components/BlogpostForm'
 import Togglable from '../components/Togglable'
+import blogImage from '../Assests/addblog.png'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles(theme => ({}))
 
 const CreateBlogForm = ({
   user,
@@ -13,6 +21,8 @@ const CreateBlogForm = ({
   handleChange,
   newBlog
 }) => {
+  const classes = useStyles()
+
   return (
     <Layout
       user={user}
@@ -33,17 +43,21 @@ const CreateBlogForm = ({
           />
         </Togglable>
       </Section>
-      <Section
-        subTitle={'Create your passion, your way.'}
-        description={'Create a unique and beautiful blog. It’s easy and free.'}
-      >
-        <Togglable buttonLabel='Add Blog'>
-          <BlogpostForm
-            onBlogSubmit={handleBlogSubmit}
-            handleChange={handleChange}
-            newBlog={newBlog}
-          />
-        </Togglable>
+      <Section>
+        <Grid container spacing={8}>
+          <Grid item xs={6}>
+            <h1>Know your audience</h1>
+            <p>
+              Find out which posts are a hit with Blogger’s built-in analytics.
+              You’ll see where your audience is coming from and what they’re
+              interested in. You can even connect your blog directly to Google
+              Analytics for a more detailed look.
+            </p>
+          </Grid>
+          <Grid item xs={6}>
+            <img src={blogImage} alt='Smiley face' height='350' width='500' />
+          </Grid>
+        </Grid>
       </Section>
     </Layout>
   )
