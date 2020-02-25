cada657fd0b8dee9b49f6832ad650c033b73c649
pbohora <pradeepbohora26@gmail.com>
2020-02-25 16:04:14 +0200

    add mobile menu for responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,11 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import MenuItem from '@material-ui/core/MenuItem'
+import MenuIcon from '@material-ui/icons/Menu'
 import Menu from '@material-ui/core/Menu'
 import { Link, Redirect, withRouter } from 'react-router-dom'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexGrow: 1
@@ -23,67 +24,112 @@ const useStyles = makeStyles({
   },
   logo: {
     flexGrow: 1
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
   }
-})
+}))
 
 export default function Navbar({ user, handleLogOut }) {
   const classes = useStyles()
   const [auth, setAuth] = useState(true)
   const [anchorEl, setAnchorEl] = useState(null)
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
 
   const handleMenu = event => {
     setAnchorEl(event.currentTarget)
   }
+  const handleLogOutAndClose = e => {
+    handleLogOut(e)
+    setAnchorEl(null)
+  }
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null)
+  }
 
   const handleClose = () => {
     setAnchorEl(null)
+    handleMobileMenuClose()
   }
 
-  return (
-    <div className={classes.root}>
-      <AppBar position='fixed' color='inherit'>
-        <Toolbar>
-          <Typography variant='h4' className={classes.logo}>
-            Blogs
-          </Typography>
-          <div className={classes.title}>
-            <Link to='/'>
-              <MenuItem className={classes.menuItem} onClick={handleClose}>
-                Home
-              </MenuItem>
-            </Link>
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget)
+  }
 
-            <Link to='/blogs'>
-              <MenuItem className={classes.menuItem} onClick={handleClose}>
-                Blogs
-              </MenuItem>
-            </Link>
-            {user && (
-              <Link to='/addblog'>
-                <MenuItem className={classes.menuItem} onClick={handleClose}>
-                  Add New Blog
-                </MenuItem>
-              </Link>
-            )}
-            <Link to='/about'>
-              <MenuItem className={classes.menuItem} onClick={handleClose}>
-                About
-              </MenuItem>
-            </Link>
-          </div>
-          {user ? (
-            <div>
-              <IconButton
-                aria-label='account of current user'
-                aria-controls='menu-appbar'
-                aria-haspopup='true'
-                onClick={handleMenu}
-                color='inherit'
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id='menu-appbar'
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={open}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+      <MenuItem onClick={handleLogOutAndClose}>Log out</MenuItem>
+    </Menu>
+  )
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id='mobile-appbar'
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <div className={classes.title}>
+        <Link to='/'>
+          <MenuItem className={classes.menuItem} onClick={handleClose}>
+            Home
+          </MenuItem>
+        </Link>
+
+        <Link to='/blogs'>
+          <MenuItem className={classes.menuItem} onClick={handleClose}>
+            Blogs
+          </MenuItem>
+        </Link>
+        {user && (
+          <Link to='/addblog'>
+            <MenuItem className={classes.menuItem} onClick={handleClose}>
+              Add New Blog
+            </MenuItem>
+          </Link>
+        )}
+        <Link to='/about'>
+          <MenuItem className={classes.menuItem} onClick={handleClose}>
+            About
+          </MenuItem>
+        </Link>
+      </div>
+      {user ? (
+        <div>
+          <IconButton
+            aria-label='account of current user'
+            aria-controls='menu-appbar'
+            aria-haspopup='true'
+            onClick={handleMenu}
+            color='inherit'
+          >
+            <AccountCircle />
+          </IconButton>
+          {/* <Menu
                 id='menu-appbar'
                 anchorEl={anchorEl}
                 anchorOrigin={{
@@ -101,22 +147,124 @@ export default function Navbar({ user, handleLogOut }) {
                 <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem>
                 <MenuItem onClick={handleLogOut}>Log out</MenuItem>
-              </Menu>
-            </div>
-          ) : (
-            <>
-              <Link to='/login'>
+              </Menu> */}
+        </div>
+      ) : (
+        <>
+          <Link to='/login'>
+            <MenuItem className={classes.menuItem} onClick={handleClose}>
+              Sign up
+            </MenuItem>
+          </Link>
+          <Link to='/login'>
+            <MenuItem className={classes.menuItem}>Login</MenuItem>
+          </Link>
+        </>
+      )}
+      <IconButton
+        edge='start'
+        className={classes.menuButton}
+        onClick={'handleMobileMenuOpen'}
+        color='inherit'
+        aria-label='open drawer'
+      >
+        <MenuIcon />
+      </IconButton>
+    </Menu>
+  )
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='fixed' color='inherit'>
+        <Toolbar>
+          <Typography variant='h4' className={classes.logo}>
+            Blogs
+          </Typography>
+          <div className={classes.sectionDesktop}>
+            <div className={classes.title}>
+              <Link to='/'>
                 <MenuItem className={classes.menuItem} onClick={handleClose}>
-                  Sign up
+                  Home
                 </MenuItem>
               </Link>
-              <Link to='/login'>
-                <MenuItem className={classes.menuItem}>Login</MenuItem>
+
+              <Link to='/blogs'>
+                <MenuItem className={classes.menuItem} onClick={handleClose}>
+                  Blogs
+                </MenuItem>
               </Link>
-            </>
-          )}
+              {user && (
+                <Link to='/addblog'>
+                  <MenuItem className={classes.menuItem} onClick={handleClose}>
+                    Add New Blog
+                  </MenuItem>
+                </Link>
+              )}
+              <Link to='/about'>
+                <MenuItem className={classes.menuItem} onClick={handleClose}>
+                  About
+                </MenuItem>
+              </Link>
+            </div>
+            {user ? (
+              <div>
+                <IconButton
+                  aria-label='account of current user'
+                  aria-controls='menu-appbar'
+                  aria-haspopup='true'
+                  onClick={handleMenu}
+                  color='inherit'
+                >
+                  <AccountCircle />
+                </IconButton>
+                {/* <Menu
+                id='menu-appbar'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleLogOut}>Log out</MenuItem>
+              </Menu> */}
+              </div>
+            ) : (
+              <>
+                <Link to='/login'>
+                  <MenuItem className={classes.menuItem} onClick={handleClose}>
+                    Sign up
+                  </MenuItem>
+                </Link>
+                <Link to='/login'>
+                  <MenuItem className={classes.menuItem}>Login</MenuItem>
+                </Link>
+              </>
+            )}
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              edge='start'
+              className={classes.menuButton}
+              onClick={handleMobileMenuOpen}
+              color='inherit'
+              aria-label='open drawer'
+            >
+              <MenuIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
     </div>
   )
 }
