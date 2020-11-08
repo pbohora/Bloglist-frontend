79d5d5e221a1cedf3ea041f48b315c39750b4653
pradipb123 <pradeepbohora26@gmail.com>
2020-11-08 20:00:55 +0200

    refactor blogs page

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -2,7 +2,25 @@ import React from "react";
 import Layout from "../components/Layout";
 import Section from "../components/Section/Section";
 import BlogList from "../components/BLogs/BlogList";
-import blogImage from "../Assests/woman.png";
+import ScrollableTab from "../components/ScrollableTab";
+import blogImage from "../Assests/office.png";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  about: {
+    alignSelf: "center",
+    width: "180px",
+    padding: "10px 40px",
+    background: "#fafafa",
+    border: "1px solid #05a0e7",
+    color: "#05a0e7",
+    "&:hover": {
+      background: "#05a0e7",
+      color: "white",
+    },
+  },
+}));
 
 const BlogPage = ({
   user,
@@ -14,6 +32,8 @@ const BlogPage = ({
   openDialog,
   handleClose,
 }) => {
+  const classes = useStyles();
+
   return (
     <Layout
       user={user}
@@ -21,13 +41,49 @@ const BlogPage = ({
       sucessMessage={sucessMessage}
       errorMessage={errorMessage}
     >
-      <Section sectionTitle="Blogs" background={true}>
-        <BlogList
-          handleLike={handleLike}
-          handleRemove={handleRemove}
-          user={user}
-          handleClose={handleClose}
-        />
+      <Section>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            // marginTop: "120px",
+            height: "82vh",
+          }}
+        >
+          <div>
+            <img src={blogImage} height="100%" width="100%" alt="img" />
+          </div>
+          <div style={{ paddingLeft: "90px" }}>
+            <h2>
+              <span style={{ color: "#05a0e7" }}>Welcome to the Blogs.</span>{" "}
+              Read your favorite blog.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation{" "}
+            </p>
+            <Button
+              size="large"
+              variant="contained"
+              type="submit"
+              className={classes.about}
+            >
+              About Us
+            </Button>
+          </div>
+        </div>
+        <div>
+          <ScrollableTab />
+          <BlogList
+            handleLike={handleLike}
+            handleRemove={handleRemove}
+            user={user}
+            handleClose={handleClose}
+          />
+        </div>
       </Section>
     </Layout>
   );
---
+++ src/components/ScrollableTab/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    color: "#05a0e7",
+    "& > span": {
+      maxWidth: 60,
+      width: "100%",
+      backgroundColor: "#05a0e7",
+    },
+  },
+})((props) => (
+  <Tabs
+    variant="scrollable"
+    {...props}
+    TabIndicatorProps={{ children: <span /> }}
+  />
+));
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: "none",
+    color: "black",
+    fontWeight: "bolder",
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    "&:focus": {
+      opacity: 1,
+    },
+    "&$selected": {
+      color: "#05a0e7",
+    },
+  },
+  selected: {},
+}))((props) => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  padding: {
+    padding: theme.spacing(1),
+  },
+}));
+
+const ScrollableTab = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.demo2}>
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <StyledTab label="Technology" />
+          <StyledTab label="Sports" />
+          <StyledTab label="Sports" />
+          <StyledTab label="Leadership" />
+          <StyledTab label="Fashion" />
+        </StyledTabs>
+        <Typography className={classes.padding} />
+      </div>
+    </div>
+  );
+};
+
+export default ScrollableTab;
